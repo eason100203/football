5a5fafdb4688f2b15e06e06a5b78eaea2e928aff
黃翊宸 <eason100203@gmail.com>
2026-05-05 17:26:05 +0800

    測試v4

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ async function handleEvent(event) {
 
 // ────────────────────────────────────────
 async function getWeeklyMatches() {
-  const now = dayjs();
+  const now = dayjs('2026-06-11');
   const startOfToday = now.startOf('day').format('YYYY-MM-DD HH:mm');
   const endOfWeek = now.add(7, 'day').endOf('day').format('YYYY-MM-DD HH:mm');
 
